583aeb240e9511eff397c2ce7a671682c28cc40e
Mussavir ul haq <mussavirulhaq@gmail.com>
2026-02-02 16:53:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.post('/save-payment', async (req, res) => {
 });
 
 //cloud connect 
-mongoose.connect('mongodb+srv://mussavir-shopkart:mussavir-shopkart@cluster0.7awjazq.mongodb.net/?appName=Cluster0')
+mongoose.connect(process.env.mongodb+srv://mussavir-shopkart:mussavir-shopkart@cluster0.7awjazq.mongodb.net/?appName=Cluster0)
 then(()=> console.log("Connected to MongoDB Atlas!"))
 .catch(err => console.error("Could not connect:",err));
 
@@ -80,3 +80,4 @@ app.listen(PORT,() => {
     console.log(`Server is running on port ${PORT}`);
     
 });
+
